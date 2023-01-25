9c726a93d3e045b99bbbb72fcd665189ea5ca9f5
Igor <igormartins.6873@gmail.com>
2023-01-25 15:55:38 -0300

    exercicios do dia 3.2

---
+++ secao-4-introducao-javascript/dia-2-javascript-arrays-loop-for/exercicios.js
@@ -0,0 +1,18 @@
+let numbers = [ 5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0 ;
+
+for (let  índice  =  0 ;  índice  <  numbers.length ; index +=  1 )  {
+    soma += numbers [ índice ] ;
+}
+
+ let media =  soma / numbers.length;
+
+ console.log( media ) ;
+
+ if  ( média  >  20 )  {
+    console.log('valor maior que 20');
+ }  else {
+    console.log('valor menor ou igual a 20')
+ }
+ 
